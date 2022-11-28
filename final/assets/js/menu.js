8a05f5f216c2242269a6b8eab91d036c98d2d904
Enso <e.ensomniacs@gmail.com>
2022-11-27 18:11:38 -0600

    additional changes

--- final/assets/js/menu.js
+++ final/assets/js/menu.js
@@ -1,3 +1,10 @@
-function toggleMobileMenu(menu) {
-    menu.classList.toggle('open');
-}
+
+$(window).on('resize', function() {
+    if($(window).width() > 900) {
+        $('#header').addClass('index');
+        $('#header').removeClass('inline');
+    }else{
+        $('#header').addClass('inline');
+        $('#header').removeClass('index');
+    }
+})
